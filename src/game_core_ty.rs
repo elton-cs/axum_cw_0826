30e7a96b77fdb3eb62fed3a998aa95c21f949f24
elton-cs <govada.elton@gmail.com>
2026-08-12 18:08:24 -0500

    feat: core types v2

--- src/game_core_ty.rs
+++ src/game_core_ty.rs
@@ -1,50 +1,49 @@
-pub struct Item {
-    pub item_type: ItemType,
-    pub player_id: usize,
-    pub player_name: String,
-    pub player_login_username: String,
-    pub player_login_password: String,
-
-    pub player_stat_gem: u32,
-    pub player_stat_exp: u32,
-    pub player_stat_lvl: u32,
-    pub player_stat_frag_count: [u32; 26],
-    pub player_stat_rune_count: [u32; 26],
-
-    pub puzzle_level: u32,
-    pub puzzle_level_next: usize,
-    pub puzzle_correct_word: [char; 5],
-
-    pub puzzle_attempt_num: u32,
-    pub puzzle_attempt_next: usize,
-    pub puzzle_attempt_word: [char; 5],
-    pub puzzle_attempt_hint: [Hint; 5],
-
-    pub puzzle_complete: bool,
-    pub puzzle_reward_exp: u32,
-    pub puzzle_reward_frag: char,
-
-    pub tile_x: u32,
-    pub tile_y: u32,
-    pub tile_frag_exp: u32,
-    pub tile_rune_exp: u32,
-    pub tile_frag_gem: u32,
-    pub tile_rune_gem: u32,
-    pub tile_frag_letter: char,
-    pub tile_rune_letter: char,
-    pub tile_frag_player_name: String,
-    pub tile_rune_player_name: String,
+pub struct Game {
+    pub user: Vec<User>,
+    pub tile: Vec<Vec<Tile>>,
 }
 
-pub enum ItemType {
-    Player,
-    PuzzleGame,
-    PuzzleGameAttempt,
-    Tile,
+pub struct User {
+    pub name: String,
+    pub pass: String,
+
+    pub gem: u32,
+    pub exp: u32,
+    pub lvl: u32,
+    pub frag_count: [u32; 26],
+    pub rune_count: [u32; 26],
+
+    pub curr_puzzle: Option<Puzzle>,
+    pub prev_puzzle: Vec<Puzzle>,
+}
+
+pub struct Puzzle {
+    pub reward_exp: u32,
+    pub reward_frag: char,
+    pub correct_word: String,
+    pub attempt_word: Vec<Attempt>,
+}
+
+pub struct Attempt {
+    pub word: String,
+    pub hint: [Hint; 5],
 }
 
 pub enum Hint {
     Missing,
     Present,
     Correct,
+}
+
+pub struct Tile {
+    pub x: u32,
+    pub y: u32,
+    pub frag_exp: u32,
+    pub rune_exp: u32,
+    pub frag_gem: u32,
+    pub rune_gem: u32,
+    pub frag_letter: Option<char>,
+    pub rune_letter: Option<char>,
+    pub frag_user_name: Option<String>,
+    pub rune_user_name: Option<String>,
 }
